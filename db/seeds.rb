333b2b047fde93e05cc9a66ea5c1e5d51f57632b
Marianna Safuler <msafuler@gmail.com>
2022-01-22 17:14:23 +0100

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Flat.destroy_all
+
+puts 'Creating locations'
+
+4.times do
+  flat = Flat.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    description: Faker::House.room,
+    price_per_night: rand(55..125),
+    number_of_guests: rand(2..10),
+    picture_url: Faker::LoremFlickr.image(search_terms: %w[hotel])
+  )
+  puts "location #{flat.id} has been created"
+end
